1e58a8634cdac77acacf699f1392f727e856bd39
MrZebo <mrzebo2@gmail.com>
2017-08-21 16:14:04 +0200

    sixth

--- Game/src/org/mrzebo/game/DIRECTION.java
+++
@@ -1,8 +0,0 @@
-package org.mrzebo.game;
-
-/**
- * Created by Home on 06.08.17.
- */
-public enum DIRECTION {
-    UP, DOWN, RIGHT, LEFT
-}
--- Game/src/org/mrzebo/game/Launcher.java
+++
@@ -1,8 +0,0 @@
-package org.mrzebo.game;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Game game = new Game("Game!", 600, 600);
-        game.start();
-    }
-}
